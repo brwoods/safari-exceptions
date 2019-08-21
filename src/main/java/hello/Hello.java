424a5d21e89ff1fb4d677e5a921294b668dc0853
Simon Roberts <simon@dancingcloudservices.com>
2019-08-21 16:45:49 -0600

    Test2

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Hello {
   public static void main(String[] args) {
-    ((Runnable)() -> System.out.println("Hello Exceptions World!")).run();
+    ((Runnable)() -> System.out.println("Hello World!")).run();
   }
 }
