2d36faa7fe851a9aafa8a23f85e4ff536851c406
Simon Roberts <simon@dancingcloudservices.com>
2019-08-22 10:27:03 -0600

    Break two

--- src/main/java/pointofsale/GetPaid.java
+++ src/main/java/pointofsale/GetPaid.java
@@ -1,5 +1,9 @@
 package pointofsale;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+
 class ModemDidNotConnectException extends Exception {
 }
 
@@ -9,22 +13,32 @@ class ModemLibrary {
   }
 }
 
+class RetryCCLaterException extends Exception {
+  public RetryCCLaterException(Throwable cause) {
+    super(cause);
+  }
+  // Should have full suite of constructors!!! Omitted for example brevity
+}
 public class GetPaid {
+  private static boolean USING_NETWORK = true;
 
   public static void authorizePayment(int cc, int amount)
-      throws ModemDidNotConnectException {
+//      throws ModemDidNotConnectException, IOException { // IMPLEMENTATION DETAIL
+      throws RetryCCLaterException /*, NoMoney, Thief */ { // NOT IMPLEMENTATION DETAIL :)
     int retries = 3;
     boolean success = false;
     while (!success) {
       try {
-        // dial modem
-        ModemLibrary.dialModem(1234561234);
-        // connect
-        // security
-        // make payment request.
-      } catch (ModemDidNotConnectException mdnce) {
+        if (USING_NETWORK) {
+          // use network
+          Socket s = new Socket("127.0.0.1", 8080);
+        } else {
+          ModemLibrary.dialModem(1234561234); // implementation detail
+        }
+        // connect, security, make payment request.
+      } catch (IOException | ModemDidNotConnectException mdnce) {
         retries--;
-        if (retries == 0) throw mdnce;
+        if (retries == 0) throw new RetryCCLaterException(mdnce);
       }
     }
   }
@@ -34,8 +48,9 @@ public class GetPaid {
     // get payment method
     try {
       authorizePayment(123, 123);
-    } catch (ModemDidNotConnectException m) {
-      
+    } catch (RetryCCLaterException m) { // Makes sense to me !
+//    } catch (ModemDidNotConnectException | IOException m) { // What is this??
+
     }
   }
 }
